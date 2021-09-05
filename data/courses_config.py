2b602c44adc37bdd5e61ff68067082426733a5fd
o.lubimova <o.lubimova@corp.mail.ru>
2021-09-05 23:12:16 +0300

    add emoji

--- data/courses_config.py
+++ data/courses_config.py
@@ -5,112 +5,286 @@ courses_dict = {
 }
 
 courses = {
-        'wardrobe': {
-            0: {
-                'title': 'День 0. Вступление.',
-                'description': '''Первый материал - подготовительный. В нем я рассказываю о том, что такое осознанный 
-                гардероб, о чем будет курс, какие сложности могут возникнуть при расхламлении и как делить вещи на 
-                нужные и ненужные. После прочтения не забудь нажать на кнопку "Готово", чтобы завтра получить первое 
-                задание!''',
-                'url': 'https://nothing-extra.ru/wardrobe-intro/'
-            },
-            1: {
-                'title': 'День 1. Диаграмма занятости',
-                'description':'''Залог хорошего гардероба - это подбор вещей под ваш образ жизни. Иначе одежда будет 
-                висеть в шкафу, вместо того чтобы гулять и жить вместе с нами. Поэтому первый шаг - это разобраться, 
-                как мы проводим свое время, чтобы следующим шагом подобрать удобные и классные вещи под наш ритм и 
-                интересы.''',
-                'url': 'http://nothing-extra.ru/day1-diagram/'
-            },
-            2: {
-                'title': 'День 2. Визуализация нашего стиля',
-                'description': '''Время мы разбирались с тем, какая одежда нам нужна для комфортной жизни. А сегодня 
-                давайте попробуем подобрать те визуальные образы и идеи, которые нам нравятся и которые мы хотим 
-                видеть в своем гардеробе.''',
-                'url': 'http://nothing-extra.ru/day2-visualization/'
-            },
-            3: {
-                'title': 'День 3. Составляем план гардеробной капсулы',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day3-capsule-and-base/'
-            },
-            4: {
-                'title': 'День 4. Куда отдать ненужное?',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day4-kuda-otdat/'
-            },
-            5: {
-                'title': 'День 5. Разбираем средний слой',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day5-6-middle-layer/'
-            },
-            6: {
-                'title': 'День 6. Все еще разбираем средний слой',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day5-6-middle-layer/'
-            },
-            7: {
-                'title': 'День 7. Воскресение',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day7-wardrobe/'
-            },
-            8: {
-                'title': 'День 8. Разбираем нижний слой',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day8-underwear/'
-            },
-            9: {
-                'title': 'День 9. Разбираем верхний слой и туфли',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day9-up-and-shoes/'
-            },
-            10: {
-                'title': 'День 10. Последняя категория для разбора - аксессуары',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day10-accessories/'
-            },
-            11: {
-                'title': 'День 11. Наконец-то собираем капсулы',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day11-12-capsules/'
-            },
-            12: {
-                'title': 'День 12. Продолжаем собирать капсулы',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day11-12-capsules/'
-            },
-            13: {
-                'title': 'День 13. Ухаживаем за вещами',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day13-repair/'
-            },
-            14: {
-                'title': 'День 14. Заключительный!',
-                'description': '''
-                ''',
-                'url': 'http://nothing-extra.ru/day14-finish/'
-            }
+    'wardrobe': {
+        0: {
+            'title': 'День 0. Вступление.',
+            'description': '''Первый материал - подготовительный. В нем я рассказываю о том, что такое осознанный 
+            гардероб, о чем будет курс, какие сложности могут возникнуть при расхламлении и как делить вещи на 
+            нужные и ненужные. После прочтения не забудь нажать на кнопку "Готово", чтобы завтра получить первое 
+            задание!''',
+            'url': 'https://nothing-extra.ru/wardrobe-intro/'
         },
+        1: {
+            'title': 'День 1. Диаграмма занятости',
+            'description':'''Залог хорошего гардероба - это подбор вещей под ваш образ жизни. Иначе одежда будет 
+            висеть в шкафу, вместо того чтобы гулять и жить вместе с нами. Поэтому первый шаг - это разобраться, 
+            как мы проводим свое время, чтобы следующим шагом подобрать удобные и классные вещи под наш ритм и 
+            интересы.''',
+            'url': 'http://nothing-extra.ru/day1-diagram/'
+        },
+        2: {
+            'title': 'День 2. Визуализация нашего стиля',
+            'description': '''Время мы разбирались с тем, какая одежда нам нужна для комфортной жизни. А сегодня 
+            давайте попробуем подобрать те визуальные образы и идеи, которые нам нравятся и которые мы хотим 
+            видеть в своем гардеробе.''',
+            'url': 'http://nothing-extra.ru/day2-visualization/'
+        },
+        3: {
+            'title': 'День 3. Составляем план гардеробной капсулы',
+            'description': '''Чтобы построить свой гардероб осознанно - всегда нужен план!
+            ''',
+            'url': 'http://nothing-extra.ru/day3-capsule-and-base/'
+        },
+        4: {
+            'title': 'День 4. Куда отдать ненужное?',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day4-kuda-otdat/'
+        },
+        5: {
+            'title': 'День 5. Разбираем средний слой',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day5-6-middle-layer/'
+        },
+        6: {
+            'title': 'День 6. Все еще разбираем средний слой',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day5-6-middle-layer/'
+        },
+        7: {
+            'title': 'День 7. Воскресение',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day7-wardrobe/'
+        },
+        8: {
+            'title': 'День 8. Разбираем нижний слой',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day8-underwear/'
+        },
+        9: {
+            'title': 'День 9. Разбираем верхний слой и туфли',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day9-up-and-shoes/'
+        },
+        10: {
+            'title': 'День 10. Последняя категория для разбора - аксессуары',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day10-accessories/'
+        },
+        11: {
+            'title': 'День 11. Наконец-то собираем капсулы',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day11-12-capsules/'
+        },
+        12: {
+            'title': 'День 12. Продолжаем собирать капсулы',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day11-12-capsules/'
+        },
+        13: {
+            'title': 'День 13. Ухаживаем за вещами',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day13-repair/'
+        },
+        14: {
+            'title': 'День 14. Заключительный!',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day14-finish/'
+        }
+    },
     'ecolife': {
-            1: {
-                'title': 'day1',
-                'url': 'url1'
-            },
-            2: {
-                'title': 'day2',
-                'url': 'url2'
-            }
+        0: {
+            'title': 'day0',
+            'description': '''
+            ''',
+            'url': ''
+        },
+        1: {
+            'title': 'day1',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/part1-1-zero-waste/'
+        },
+        2: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-2-recycling-points/'
+        },
+        3: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-3-fridge/'
+        },
+        4: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-4-kitchen/'
+        },
+        5: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-5-packets/'
+        },
+        6: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-6-sweets/'
+        },
+        7: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-7-relax/'
+        },
+        8: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-mindfull-nutrition/'
+        },
+        9: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-organic-cosmetic/'
+        },
+        10: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-kosmetika-uhodi/'
+        },
+        11: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-decorative-cosmetic/'
+        },
+        12: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-chemistry/'
+        },
+        13: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-pharmacy/'
+        },
+        14: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/part1-relax-14/'
+        }
+    },
+    'digital': {
+        0: {
+            'title': 'day0',
+            'description': '''
+            ''',
+            'url': 'http://nothing-extra.ru/day0-digital-minimalism/'
+        },
+        1: {
+            'title': 'day1',
+            'description': '''
+            ''',
+            'url': 'https://nothing-extra.ru/day1-time/'
+        },
+        2: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day3-social-nets/'
+        },
+        3: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day3-social-nets/'
+        },
+        4: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day4-weakness-moments/'
+        },
+        5: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day5-calm-down/'
+        },
+        6: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day6-lent/'
+        },
+        7: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day7-email/'
+        },
+        8: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day8-messages/'
+        },
+        9: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day9-communication/'
+        },
+        10: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day10-gallery/'
+        },
+        11: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day11-apps-downloads/'
+        },
+        12: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day12-not-trash/'
+        },
+        13: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day13-boredom/'
+        },
+        14: {
+            'title': 'day0',
+            'description': '''
+                            ''',
+            'url': 'http://nothing-extra.ru/day14-peace/'
+        }
     }
 }
 
