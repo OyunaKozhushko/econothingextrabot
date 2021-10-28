06b7366a4ba0ee46d78ffd6f5ee990a5eaea5260
o.lubimova <o.lubimova@corp.mail.ru>
2021-10-28 22:49:46 +0300

    add courses description

---
+++ data/answers.py
@@ -0,0 +1,8 @@
+from .emoji import emoji
+
+answers = [
+    "Ты молодец! Завтра пришлю новый материал! Хорошего дня " + emoji.get('relieved_face'),
+    "Так держать! ",
+    "Супер! ",
+    "Хороший темп " + emoji.get('')
+]
--- data/emoji.py
+++ data/emoji.py
@@ -1,7 +1,48 @@
+# https://unicode.org/emoji/charts/full-emoji-list.html
 emoji = {
     "hug": u"\U0001F917",
-    "relieved_face": u"\U0001F60C",
+    "relieved_face": u"\U0001F60C", # умиротворенный
     "down_arrow": u"\u2193",
     "relaxed_face": u"\u263A",
-    "pensive_face": u"\U0001F614"
+    "pensive_face": u"\U0001F614", # грустноватый
+    "upside_down_face": u"\U0001F643", # вверхногами
+    "smilling_face": u"\U0000263A", # с прикрытыми глазами
+    "fire": u"\U0001F525",
+    "party_popper": u"\U0001F389", # конфетти
+    "heart": u"\U00002764",
+    "calendar": u"\U0001F5D3",
+    "green_mark": u"\U00002705",
+    "biceps": u"\U0001F4AA",
+    "raising_hands": u"\U0001F64C",
+    "applause": u"\U0001F44F",
+    "thumb_up": u"\U0001F44D",
+    "love_you_gesture": u"\U0001F91F",
+    "victory_hand": u"\U0000270C",
+    "wave": u"\U0001F44B",
+    "hundred": u"\U0001F4AF",
+    "pleading_face": u"\U0001F97A",
+    "flushed_face": u"\U0001F633",
+    "raised_eyebrow": u"\U0001F928",
+    "face_savoring_food": u"\U0001F60B", # улыбается с языком
+    "star_eyes_face": u"\U0001F929",
+    "heart_eyes_face": u"\U0001F60D",
+    "hearts_face": u"\U0001F970",
+    "winking_face": u"\U0001F609", # подмигивает
+    "grinning_face": u"\U0001F601", # широко улыбается,
+    "shopping_bags": u"\U0001F6CD",
+    "lipstick": u"\U0001F484",
+    "t_shirt": u"\U0001F455",
+    "jeans": u"\U0001F456",
+    "coat": u"\U0001F9E5",
+    "bikini": u"\U0001F459",
+    "backpack": u"\U0001F392",
+    "boots": u"\U0001F97E",
+    "sunglasses": u"\U0001F576",
+    "watch": u"\U0000231A",
+    "fishing_pole": u"\U0001F3A3",
+    "wastebasket": u"\U0001F5D1",
+    "infinity": u"\U0000267E",
+    "postbox": u"\U0001F4EE",
+    "memo": u"\U0001F4DD"
+
 }
